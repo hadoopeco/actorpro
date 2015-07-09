8f69fdea369c203273bf7b9bab602ca0e4d905e7
hadoopeco <wbmark@gmail.com>
2015-07-09 16:19:49 +0800

    add main class

---
+++ src/main/java/OkMain.java
@@ -0,0 +1,11 @@
+import actor.OkCoinSystem;
+
+/**
+ * Created by lenovo on 2015/7/9.
+ */
+public class OkMain {
+
+    public static void main(String[] args) {
+        OkCoinSystem.main(args);
+    }
+}
