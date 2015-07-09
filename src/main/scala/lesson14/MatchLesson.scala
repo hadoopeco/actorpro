3016773e6a1327d0e1230b1b3393fc2988f3a133
hadoopeco <wbmark@gmail.com>
2015-07-09 15:59:39 +0800

    add assembly package

--- src/main/scala/lesson14/MatchLesson.scala
+++ src/main/scala/lesson14/MatchLesson.scala
@@ -5,4 +5,36 @@ package lesson14
  */
 class MatchLesson {
 
+
+}
+
+object MatchLesson extends App{
+  var ch:Char = '3'
+  var sign = ch match {
+    case '_' => 1
+    case '+' => 2
+    case _ if Character.isDigit(ch) => Character.digit(ch,10)
+    case _ => 0
+  }
+  println(sign)
+
+  val pattern = "([0-9]+) ([a-z]+)".r
+
+  "99 items" match {
+    case pattern(num,item) => println(num,item)
+    case _ => println("test")
+  }
+
+  val arra = Array(0,2)
+
+  arra match {
+    case Array(0) => println("array is 0")
+    case Array(x,y) => println("array is x,y")
+    case Array(0,_*) => println("array is 0 ,*")
+    case _ => println("other")
+  }
+
+
+
+
 }
